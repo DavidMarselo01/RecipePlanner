09d1e3482a3f9cf6f8ba49c44d4924ea950e0a75
DrEvilBrain <drevilbrain@gmail.com>
2021-04-24 17:43:25 -0400

    Add bootstrap to the webpages

--- RecipePlanner.py
+++ RecipePlanner.py
@@ -18,7 +18,7 @@ app.secret_key = "super secret key"
 IMAGES_DIR = os.path.join(os.getcwd(), "images")
 
 connection = pymysql.connect(host="127.0.0.1",
-                             user="root",
+                             user="drevilbrain",
                              password="",
                              db="RecipePlanner",
                              charset="utf8mb4",
@@ -95,7 +95,7 @@ def loginAuth():
         hashedPassword = hashlib.sha256(plaintextPasword.encode("utf-8")).hexdigest()
 
         with connection.cursor() as cursor:
-            queryLogin = "SELECT * FROM person WHERE username = %s AND password = %s"
+            queryLogin = "SELECT * FROM Person WHERE username = %s AND password = %s"
             cursor.execute(queryLogin, (username, hashedPassword))
             
         data = cursor.fetchone()
