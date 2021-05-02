dd3ff9ad29720b5e7b3adbbf88c039087962a409
James Franco <theoriginaljamesfranco@gmail.com>
2021-05-02 15:37:30 -0400

    some ingredients & recipes. upped char limit for recipe instructions

---
+++ db/Recipes.sql
@@ -0,0 +1,137 @@
+INSERT INTO Recipes (recipe_name, cook_time, instructions)
+VALUES ('Cacio e Pepe', 15,
+'1. Bring pot of salted water to a boil, add spaghetti.
+2. Cook spaghetti to desired tenderness, then drain, reserving some pasta water, and move spaghetti to pan.
+3. Grate Pecorino Romano over pasta. Then, add a splash of pasta water and stire.
+4. Continue adding cheese & pasta water while stirring. Eventually a creamy sauce should form.
+5. Generously add black pepper and stir.
+6. Serve on plates or bowls. Serves 2 or 3.');
+
+INSERT INTO RecipeIngredients (ingredient_name, recipe_name, quantity)
+VALUES ('Spaghetti', 'Cacio e Pepe', 8);
+INSERT INTO RecipeIngredients (ingredient_name, recipe_name, quantity)
+VALUES ('Pecorino Romano', 'Cacio e Pepe', 2);
+INSERT INTO RecipeIngredients (ingredient_name, recipe_name, quantity)
+VALUES ('Salt', 'Cacio e Pepe', 0);
+INSERT INTO RecipeIngredients (ingredient_name, recipe_name, quantity)
+VALUES ('Black pepper', 'Cacio e Pepe', 0);
+
+INSERT INTO Recipes (recipe_name, cook_time, instructions)
+VALUES ('Garlic Noodles', 15,
+'1. Bring pot of water to a boil. Sauce will be very salty, so no need to salt the pasta water. Add spaghetti.
+2. While spaghetti cooks, crush and mince garlic. 
+3. Melt butter in a pan over medium heat. Add garlic and cook around 2 minutes. Garlic should be fragrant but not brown.
+4. Add a tablespoon of oyster sauce, 2 teaspoons of soy sauce, and 2 teaspoons of fish sauce to the pan and mix.
+5. As soon as noodles are done, drain, reserving some pasta water, and add to pan. Toss until noodles are throughly coated. Add small amounts of pasta water until the sauce is creamy and not overly greasy-looking.
+6. Optionally top with grated parmesan or pecorino, and scallions. Serve.');
+
+INSERT INTO RecipeIngredients (ingredient_name, recipe_name, quantity)
+VALUES ('Spaghetti', 'Garlic Noodles', 8);
+INSERT INTO RecipeIngredients (ingredient_name, recipe_name, quantity)
+VALUES ('Garlic', 'Garlic Noodles', 12);
+INSERT INTO RecipeIngredients (ingredient_name, recipe_name, quantity)
+VALUES ('Unsalted butter', 'Garlic Noodles', 2);
+INSERT INTO RecipeIngredients (ingredient_name, recipe_name, quantity)
+VALUES ('Oyster sauce', 'Garlic Noodles', 0);
+INSERT INTO RecipeIngredients (ingredient_name, recipe_name, quantity)
+VALUES ('Soy sauce', 'Garlic Noodles', 0);
+INSERT INTO RecipeIngredients (ingredient_name, recipe_name, quantity)
+VALUES ('Fish sauce', 'Garlic Noodles', 0);
+
+INSERT INTO Recipes (recipe_name, cook_time, instructions)
+VALUES ('Napolitan', 30,
+'1. Mince garlic. Thinly slice the onion, sausages, bell pepper, and mushrooms.
+2. Bring pot of salted water to a boil.
+3. Add pasta to water.
+4. Heat 2 tbsp olive oil in pan over medium heat. Add garlic, sautee for 1 minute.
+5. Add onions and season with salt & pepper. Cook for 2-3 minutes or until wilted.
+6. Add sausages and cook for another minute.
+7. Add bell peppers and mushrooms, season with salt & pepper. Continue cooking until all ingredients are cooked to the desired texture.
+8. Add 4 tbsp ketchup, a teaspoon of worcestershire sauce, and a pinch of sugar to the pan. Mix thoroughly to coat the ingredients.
+9. As soon as pasta is ready, drain, reserving some pasta water, and add noodles to the pan. Turn off heat.
+10. Add 2 tbsp milk, and mix everything in the pan thoroughly. Add small amounts of pasta water until a creamy sauce that easily coats the noodles and ingredients is achieved. Serve.');
+
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Napolitan', 'Spaghetti', 0);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Napolitan', 'Garlic', 1);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Napolitan', 'Onion', 0.5);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Napolitan', 'Button mushroom', 3.5);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Napolitan', 'Green bell pepper', 1);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Napolitan', 'Italian sausage', 2);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Napolitan', 'Olive oil', 0);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Napolitan', 'Milk', 0);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Napolitan', 'Ketchup', 0);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Napolitan', 'Worcestershire sauce', 0);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Napolitan', 'Sugar', 0);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Napolitan', 'Salt', 0);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Napolitan', 'Black pepper', 0);
+
+INSERT INTO Recipes (recipe_name, cook_time, instructions)
+VALUES ('Crispy & Fluffy Roast Potatoes', 80,
+'1. Preheat oven to 450F or 230C.
+2. Peel potatoes and cut into medium size chunks, about 1.5 inches in length. If the pieces are too small, they can fall apart in later stages, so be careful.
+3. Bring 2 liters of water to boil in a pot. Add 2 tablespoons kosher salt (less if using fine salt) and the baking soda.
+4. Add potatoes to the water, return to a boil, then simmer until a butterknife easily enters the pieces, around 10 minutes after returning to a boil.
+5. While potatoes boil, crush and mince the garlic. Heat 5 tablespoons of olive oil in a saucepan, and add garlic.
+6. Cook while stirring until garlic is just beginning to turn golden.
+7. Strain oil, reserving garlic bits for later.
+8. When potatoes are ready, drain gently (the potato chunks will be weak by this point), then return potatoes to the empty pot and let dry for 30 seconds.
+9. Toss potatoes gently in infused oil. Also add salt and pepper to taste while tossing. The goal is to create a layer of mashed-potato-like paste on the outside of the pieces, as this will become a crispy exterior during baking.
+10. Move potatoes to a baking sheet and spread out evenly.
+11. Bake potatoes for 20 minutes without moving them. Then, flip and turn potatoes and return to oven. Repeat every 15-20 minutes until potatoes are the desired darkness (typically 45 minutes to 1 hour total).
+12. Remove potatoes from oven. Toss with reserved garlic, and add more salt or pepper if desired. Serve.');
+
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Crispy & Fluffy Roast Potatoes', 'Russet potato', 70);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Crispy & Fluffy Roast Potatoes', 'Olive oil', 0);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Crispy & Fluffy Roast Potatoes', 'Baking soda', 0);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Crispy & Fluffy Roast Potatoes', 'Garlic', 3);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Crispy & Fluffy Roast Potatoes', 'Salt', 0);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Crispy & Fluffy Roast Potatoes', 'Black pepper', 0);
+
+INSERT INTO Recipes (recipe_name, cook_time, instructions)
+VALUES ('Menemen', 20,
+'1. Finely dice onion and long green pepper.
+2. Chop tomatoes. Alternatively, grate tomatoes using large side of a box grater.
+3. Heat 3 tablespoons olive oil over low heat. Once barely warm, add paprika, onion, and long green pepper. Season generously with salt and pepper.
+4. Cook, stirring frequently, until vegetables are very soft (around 8 minutes).
+5. Add tomatoes and continue to cook & stir until color deepens.
+6. Crack in eggs. Gently break the yolks and spread the eggs through the mixture slowly. This will create streaks of egg throughout the dish. Beating the eggs completely will create more of a scrambled egg texture, which usually is not wanted for this dish.
+7. Season again with salt and pepper.
+8. Continue to slowly cook, stirring occasionally, until eggs are mostly set.
+9. Remove from heat. Eggs will continue to set from residual heat.
+10. Optionally top with cheese. Serve.');
+
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Menemen', 'Black pepper', 0);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Menemen', 'Salt', 0);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Menemen', 'Eggs', 4);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Menemen', 'Tomato', 2);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Menemen', 'Poblano pepper', 0.5);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Menemen', 'Onion', 1);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Menemen', 'Paprika', 1);
+INSERT INTO RecipeIngredients (recipe_name, ingredient_name, quantity)
+VALUES ('Menemen', 'Olive oil', 0);
